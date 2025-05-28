3f9f5db952fa23136c8e3a0c285be45dd274c40d
arufanov <arufanov@gmail.com>
2025-05-28 12:21:52 +0300

    The bearer auth has been added for open api

--- src/main/java/com/h5radar/radar/config/OpenApiConfiguration.java
+++ src/main/java/com/h5radar/radar/config/OpenApiConfiguration.java
@@ -1,9 +1,9 @@
 package com.h5radar.radar.config;
 
-// import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.security.SecurityRequirement;
-// import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,17 +12,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class OpenApiConfiguration implements WebMvcConfigurer {
 
-  /*
   private SecurityScheme createApiKeyScheme() {
     return new SecurityScheme().type(SecurityScheme.Type.HTTP)
         .bearerFormat("JWT")
         .scheme("bearer");
-  }*/
+  }
 
   @Bean
   public OpenAPI openApi() {
-    return new OpenAPI()/*.addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-        .components(new Components().addSecuritySchemes("Bearer Authentication", createApiKeyScheme()))*/;
+    return new OpenAPI().addSecurityItem(new SecurityRequirement()
+            .addList("Bearer Authentication"))
+        .components(new Components()
+            .addSecuritySchemes("Bearer Authentication", createApiKeyScheme()));
   }
 
   /* TODO:
